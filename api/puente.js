d711ef56bc5cdad6b737a50c2269827ed5e2ae84
cdolatam <invitaciones@cdo-latam.org>
2025-06-04 13:31:52 -0500

    puente.js

--- api/puente.js
+++ api/puente.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).send('Only POST requests are allowed.');
   }
 
-  const response = await fetch(https://defaulte69929079be64dcba9310bbf893f6e.22.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/9167a9c6b7954a3ca34ee739f99b8e05/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=Default-e6992907-9be6-4dcb-a931-0bbf893f6e22, {
+  const response = await fetch("https://defaulte69929079be64dcba9310bbf893f6e.22.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/9167a9c6b7954a3ca34ee739f99b8e05/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=Default-e6992907-9be6-4dcb-a931-0bbf893f6e22", {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
