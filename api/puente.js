2a4f349ed02a8b34a8944356772f2c80b6703ef5
cdolatam <invitaciones@cdo-latam.org>
2025-06-04 14:54:30 -0500

    puente.js

--- api/puente.js
+++ api/puente.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     console.log('Body recibido:', req.body); // LOG del cuerpo entrante
 
     const response = await fetch(
-      'https://defaulte69929079be64dcba9310bbf893f6e.22.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/9167a9c6b7954a3ca34ee739f99b8e05/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=Default-e6992907-9be6-4dcb-a931-0bbf893f6e22',
+      'https://defaulte69929079be64dcba9310bbf893f6e.22.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/7758822e615547268445409218885942/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=Default-e6992907-9be6-4dcb-a931-0bbf893f6e22&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DKby3gi8IRtoE74aY_8v1Ltui5XjzJzFDjKFU4tAbB4',
       {
         method: 'POST',
         headers: {
