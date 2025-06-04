8ec67a5c600dc8c3c9e8e193124f0f588799079f
cdolatam <invitaciones@cdo-latam.org>
2025-06-04 13:21:16 -0500

    Create puente.js

---
+++ api/puente.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Only POST requests are allowed.');
+  }
+
+  const response = await fetch(https://defaulte69929079be64dcba9310bbf893f6e.22.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/9167a9c6b7954a3ca34ee739f99b8e05/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=Default-e6992907-9be6-4dcb-a931-0bbf893f6e22, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req.body),
+  });
+
+  const result = await response.text();
+  res.status(200).send(`Webhook forwarded:\n${result}`);
+}
