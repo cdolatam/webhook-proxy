003fe7e2cd95cef318a5d1184d19305baffac1b6
cdolatam <invitaciones@cdo-latam.org>
2025-06-04 14:24:10 -0500

    puente.js

--- api/puente.js
+++ api/puente.js
@@ -4,8 +4,10 @@ export default async function handler(req, res) {
   }
 
   try {
-    const powerAutomateResponse = await fetch(
-      'https://defaulte69929079be64dcba9310bbf893f6e.22.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/9167a9c6b7954a3ca34ee739f99b8e05/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=Default-e6992907-9be6-4dcb-a931-0bbf893f6e22', // usa aquí la URL exacta de tu flujo
+    console.log('Body recibido:', req.body); // LOG del cuerpo entrante
+
+    const response = await fetch(
+      'https://defaulte69929079be64dcba9310bbf893f6e.22.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/9167a9c6b7954a3ca34ee739f99b8e05/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=Default-e6992907-9be6-4dcb-a931-0bbf893f6e22',
       {
         method: 'POST',
         headers: {
@@ -15,11 +17,10 @@ export default async function handler(req, res) {
       }
     );
 
-    const result = await powerAutomateResponse.text();
-
+    const result = await response.text();
     res.status(200).send(`Webhook forwarded:\n${result}`);
   } catch (error) {
-    console.error('Error sending to Power Automate:', error);
+    console.error('Error:', error);
     res.status(500).send('Failed to forward to Power Automate');
   }
 }
